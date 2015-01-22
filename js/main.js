4f789e199576b6026b33ea9fa5d553d181c1722f
Joshua Raichur <hello@joshuaraichur.com>
2015-01-22 20:42:42 +0530

    dribbble posts

--- js/main.js
+++ js/main.js
@@ -49,3 +49,19 @@ if($('body').hasClass('code')){
       $('time').timeago();
   });
 }
+
+if($('body').hasClass('pixels')){
+  $.jribbble.getShotsByPlayerId('geek', function (playerShots) {
+    var html = [];
+
+    $.each(playerShots.shots, function (i, shot) {
+      html.push('<li><h3>' + shot.title + '</h3>');
+      html.push('<h4>by ' + shot.player.name + '</h4><a href="' + shot.url + '">');
+      html.push('<img src="' + shot.image_teaser_url + '" ');
+      html.push('alt="' + shot.title + '"></a></li>');
+    });
+
+    $('#pixelslist').html(html.join(''));
+  }, {page: 1, per_page: 20});
+
+}
