6426a3ade7ce6b1ae367001fa20ae35e20eedea3
Joshua Raichur <hello@joshuaraichur.com>
2015-01-24 07:13:44 +0530

    Refactor js and filter list

--- js/main.js
+++ js/main.js
@@ -1,7 +1,45 @@
+// Get Data functions
+function getDribbbleData(){
+  $.jribbble.getShotsByPlayerId('geek', function (playerShots) {
+    var html = [], date;
+    $.each(playerShots.shots, function (i, shot) {
+      date = new Date(shot.created_at).toISOString();
+      html.push('<li><h3 class="name">' + shot.title + '</h3>');
+      html.push('<img src="' + shot.image_teaser_url + '" ');
+      html.push('alt="' + shot.title + '">');
+      html.push('<time datetime="' + date + '">' + date + '</time></a></li>');
+    });
+
+    $('#pixelslist').html(html.join(''));
+  }, {page: 1, per_page: 20});
+}
+
+function getInstagramData(){
+  $('#lightlist').append('<div id="load-more">Load More</div>');
+  var loadButton = document.getElementById('load-more');
+  var feed = new Instafeed({
+    get: 'user',
+    target: 'lightlist',
+    resolution: 'low_resolution',
+    userId: 1508254017,
+    limit: 20,
+    accessToken: '1508254017.467ede5.4d8570b3606645bfa2859e1d1c54f8f1',
+    template: '<li><a href="{{link}}"><img src="{{image}}" /></a><p class="name">{{caption}}</p><p><3 {{likes}}</p></li>',
+    after: function() {
+      if (!this.hasNext()) {
+        loadButton.setAttribute('disabled', 'disabled');
+      }
+    },
+  });
+  loadButton.addEventListener('click', function() {
+    feed.next();
+  });
+  feed.run();
+}
+
 function getGithubData(){
   var reqURI = 'https://api.github.com/users/raichur',
   repoURI = 'https://api.github.com/users/raichur/repos';
-
   function requestJSON(url, callback) {
     $.ajax({
       url: url,
@@ -10,102 +48,52 @@ function getGithubData(){
       }
     });
   }
-
   requestJSON(reqURI, function(github_JSON) {
-      var repositories, output = '', repo_colors = {};
-      requestJSON('https://cdn.rawgit.com/raichur/github-language-colors/master/colors.json', function(data){
-        repo_colors = data;
-      });
+    var repositories, output = '', repo_colors = {};
+    requestJSON('https://cdn.rawgit.com/raichur/github-language-colors/master/colors.json', function(data){
+      repo_colors = data;
+    });
 
-      $.getJSON(repoURI, function(github_JSON) {
-        repositories = github_JSON;
-        outputPageContent();
-      });
+    $.getJSON(repoURI, function(github_JSON) {
+      repositories = github_JSON;
+      outputPageContent();
+    });
 
-      function outputPageContent() {
-        $.each(repositories, function(index) {
-          output += '<li>';
-          output += '<a href="' + repositories[index].html_url + '" target="_blank"><h3 class="name">' + repositories[index].name + '</h3></a>';
-          if (repositories[index].description) { output += '<p class="description">' + repositories[index].description + '</p>'; }
-          if (repositories[index].homepage) { output += '<a href="' + repositories[index].homepage + '">Live</a>'; }
-          if (repositories[index].language) { output += '<p class="language" style="color: ' + repo_colors[repositories[index].language] + '">' + repositories[index].language + '</p>'; }
-          output += '<time class="date" datetime="' + repositories[index].updated_at + '">' + repositories[index].updated_at + '</time>';
-          output += '</li>';
-        });
-        $('#codelist').html(output);
-      }
+    function outputPageContent() {
+      $.each(repositories, function(index) {
+        output += '<li>';
+        output += '<a href="' + repositories[index].html_url + '" target="_blank"><h3 class="name">' + repositories[index].name + '</h3></a>';
+        if (repositories[index].description) { output += '<p>' + repositories[index].description + '</p>'; }
+        if (repositories[index].homepage) { output += '<a href="' + repositories[index].homepage + '">Live</a>'; }
+        if (repositories[index].language) { output += '<p style="color: ' + repo_colors[repositories[index].language] + '">' + repositories[index].language + '</p>'; }
+        output += '<time datetime="' + repositories[index].updated_at + '">' + repositories[index].updated_at + '</time>';
+        output += '</li>';
+      });
+      $('#codelist').html(output);
+    }
   });
 }
 
-
-  getGithubData();
-  $(document).ajaxComplete(function() {
-      var codelistoptions = {
-        valueNames: ['name', 'language', 'date', 'description'],
-        plugins: [ ListFuzzySearch() ]
-      };
-      var codelist = new List('code', codelistoptions);
-      codelist.sort('date', { order: "desc"});
-      $('time').timeago();
-  });
-
-  $.jribbble.getShotsByPlayerId('geek', function (playerShots) {
-    var html = [], date;
-    $.each(playerShots.shots, function (i, shot) {
-      date = new Date(shot.created_at).toISOString();
-      html.push('<li><h3 class="name">' + shot.title + '</h3>');
-      html.push('<img src="' + shot.image_teaser_url + '" ');
-      html.push('alt="' + shot.title + '">');
-      html.push('<time class="date" datetime="' + date + '">' + date + '</time></a></li>');
+function filterList(filter){
+  filter.forEach(function(filter){
+    $('.' + filter + 'filter').click(function(){
+      $('#' + filter + 'list').fadeIn();
+      $('ul[id!=' + filter + 'list]').fadeOut();
     });
-
-    $('#pixelslist').html(html.join(''));
-  }, {page: 1, per_page: 20});
-  $(document).ajaxComplete(function() {
-    var pixelslistoptions = {
-      valueNames: ['name', 'date']
-    };
-    var pixelslist = new List('pixels', pixelslistoptions);
-    pixelslist.sort('date', { order: "desc"});
-    $('time').timeago();
   });
+}
 
-
-  // Instafeed
-  (function(){var e,t;e=function(){function e(e,t){var n,r;this.options={target:"instafeed",get:"popular",resolution:"thumbnail",sortBy:"none",links:!0,mock:!1,useHttp:!1};if(typeof e=="object")for(n in e)r=e[n],this.options[n]=r;this.context=t!=null?t:this,this.unique=this._genKey()}return e.prototype.hasNext=function(){return typeof this.context.nextUrl=="string"&&this.context.nextUrl.length>0},e.prototype.next=function(){return this.hasNext()?this.run(this.context.nextUrl):!1},e.prototype.run=function(t){var n,r,i;if(typeof this.options.clientId!="string"&&typeof this.options.accessToken!="string")throw new Error("Missing clientId or accessToken.");if(typeof this.options.accessToken!="string"&&typeof this.options.clientId!="string")throw new Error("Missing clientId or accessToken.");return this.options.before!=null&&typeof this.options.before=="function"&&this.options.before.call(this),typeof document!="undefined"&&document!==null&&(i=document.createElement("script"),i.id="instafeed-fetcher",i.src=t||this._buildUrl(),n=document.getElementsByTagName("head"),n[0].appendChild(i),r="instafeedCache"+this.unique,window[r]=new e(this.options,this),window[r].unique=this.unique),!0},e.prototype.parse=function(e){var t,n,r,i,s,o,u,a,f,l,c,h,p,d,v,m,g,y,b,w,E,S;if(typeof e!="object"){if(this.options.error!=null&&typeof this.options.error=="function")return this.options.error.call(this,"Invalid JSON data"),!1;throw new Error("Invalid JSON response")}if(e.meta.code!==200){if(this.options.error!=null&&typeof this.options.error=="function")return this.options.error.call(this,e.meta.error_message),!1;throw new Error("Error from Instagram: "+e.meta.error_message)}if(e.data.length===0){if(this.options.error!=null&&typeof this.options.error=="function")return this.options.error.call(this,"No images were returned from Instagram"),!1;throw new Error("No images were returned from Instagram")}this.options.success!=null&&typeof this.options.success=="function"&&this.options.success.call(this,e),this.context.nextUrl="",e.pagination!=null&&(this.context.nextUrl=e.pagination.next_url);if(this.options.sortBy!=="none"){this.options.sortBy==="random"?d=["","random"]:d=this.options.sortBy.split("-"),p=d[0]==="least"?!0:!1;switch(d[1]){case"random":e.data.sort(function(){return.5-Math.random()});break;case"recent":e.data=this._sortBy(e.data,"created_time",p);break;case"liked":e.data=this._sortBy(e.data,"likes.count",p);break;case"commented":e.data=this._sortBy(e.data,"comments.count",p);break;default:throw new Error("Invalid option for sortBy: '"+this.options.sortBy+"'.")}}if(typeof document!="undefined"&&document!==null&&this.options.mock===!1){a=e.data,this.options.limit!=null&&a.length>this.options.limit&&(a=a.slice(0,this.options.limit+1||9e9)),n=document.createDocumentFragment(),this.options.filter!=null&&typeof this.options.filter=="function"&&(a=this._filter(a,this.options.filter));if(this.options.template!=null&&typeof this.options.template=="string"){i="",o="",l="",v=document.createElement("div");for(m=0,b=a.length;m<b;m++)s=a[m],u=s.images[this.options.resolution].url,this.options.useHttp||(u=u.replace("http://","//")),o=this._makeTemplate(this.options.template,{model:s,id:s.id,link:s.link,image:u,caption:this._getObjectProperty(s,"caption.text"),likes:s.likes.count,comments:s.comments.count,location:this._getObjectProperty(s,"location.name")}),i+=o;v.innerHTML=i,S=[].slice.call(v.childNodes);for(g=0,w=S.length;g<w;g++)h=S[g],n.appendChild(h)}else for(y=0,E=a.length;y<E;y++)s=a[y],f=document.createElement("img"),u=s.images[this.options.resolution].url,this.options.useHttp||(u=u.replace("http://","//")),f.src=u,this.options.links===!0?(t=document.createElement("a"),t.href=s.link,t.appendChild(f),n.appendChild(t)):n.appendChild(f);document.getElementById(this.options.target).appendChild(n),r=document.getElementsByTagName("head")[0],r.removeChild(document.getElementById("instafeed-fetcher")),c="instafeedCache"+this.unique,window[c]=void 0;try{delete window[c]}catch(x){}}return this.options.after!=null&&typeof this.options.after=="function"&&this.options.after.call(this),!0},e.prototype._buildUrl=function(){var e,t,n;e="https://api.instagram.com/v1";switch(this.options.get){case"popular":t="media/popular";break;case"tagged":if(typeof this.options.tagName!="string")throw new Error("No tag name specified. Use the 'tagName' option.");t="tags/"+this.options.tagName+"/media/recent";break;case"location":if(typeof this.options.locationId!="number")throw new Error("No location specified. Use the 'locationId' option.");t="locations/"+this.options.locationId+"/media/recent";break;case"user":if(typeof this.options.userId!="number")throw new Error("No user specified. Use the 'userId' option.");if(typeof this.options.accessToken!="string")throw new Error("No access token. Use the 'accessToken' option.");t="users/"+this.options.userId+"/media/recent";break;default:throw new Error("Invalid option for get: '"+this.options.get+"'.")}return n=""+e+"/"+t,this.options.accessToken!=null?n+="?access_token="+this.options.accessToken:n+="?client_id="+this.options.clientId,this.options.limit!=null&&(n+="&count="+this.options.limit),n+="&callback=instafeedCache"+this.unique+".parse",n},e.prototype._genKey=function(){var e;return e=function(){return((1+Math.random())*65536|0).toString(16).substring(1)},""+e()+e()+e()+e()},e.prototype._makeTemplate=function(e,t){var n,r,i,s,o;r=/(?:\{{2})([\w\[\]\.]+)(?:\}{2})/,n=e;while(r.test(n))i=n.match(r)[1],s=(o=this._getObjectProperty(t,i))!=null?o:"",n=n.replace(r,""+s);return n},e.prototype._getObjectProperty=function(e,t){var n,r;t=t.replace(/\[(\w+)\]/g,".$1"),r=t.split(".");while(r.length){n=r.shift();if(!(e!=null&&n in e))return null;e=e[n]}return e},e.prototype._sortBy=function(e,t,n){var r;return r=function(e,r){var i,s;return i=this._getObjectProperty(e,t),s=this._getObjectProperty(r,t),n?i>s?1:-1:i<s?1:-1},e.sort(r.bind(this)),e},e.prototype._filter=function(e,t){var n,r,i,s,o;n=[],i=function(e){if(t(e))return n.push(e)};for(s=0,o=e.length;s<o;s++)r=e[s],i(r);return n},e}(),t=typeof exports!="undefined"&&exports!==null?exports:window,t.Instafeed=e}).call(this);
-
-  $('#lightlist').append('<div id="load-more">Load More</div>');
-  var loadButton = document.getElementById('load-more');
-  var feed = new Instafeed({
-    get: 'user',
-    target: 'lightlist',
-    resolution: 'low_resolution',
-    userId: 1508254017,
-    limit: 30,
-    accessToken: '1508254017.467ede5.4d8570b3606645bfa2859e1d1c54f8f1',
-    template: '<li><a href="{{link}}"><img src="{{image}}" /></a><p class="name">{{caption}}</p><p><3 {{likes}}</p><abbr class="date instafeed">{{model.created_time}}</abbr></li>',
-    after: function() {
-      // disable button if no more results to load
-      if (!this.hasNext()) {
-        loadButton.setAttribute('disabled', 'disabled');
-      }
-    },
-  });
-
-  // bind the load more button
-  loadButton.addEventListener('click', function() {
-    feed.next();
-  });
-
-  // run our feed!
-  feed.run();
-  $(document).ajaxComplete(function(){
-    $('abbr').each(function(){ this.setAttribute('title', new Date(parseInt((this.innerHTML).toString() + '000')).toISOString()); });
-    $('abbr').timeago();
-  });
-
-  var wordslistoptions = {
-    valueNames: ['name', 'date']
-  };
-  var wordslist = new List('words', wordslistoptions);
+// Getting the data from services when the page loads
+$(function(){
+  getGithubData();
+  getInstagramData();
+  getDribbbleData();
+  $('abbr').timeago();
   $('time').timeago();
+  $(".social").switcher();
+  filterList(['code', 'light', 'words', 'pixels']);
+});
+
+$(document).ajaxComplete(function() {
+  $('time').timeago();
+});
