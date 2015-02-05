f57c306b668f4672262351dec18a3862d61285cf
Joshua Raichur <hello@joshuaraichur.com>
2015-02-05 09:21:31 +0530

    greeting

--- resources/js/main.js
+++ resources/js/main.js
@@ -343,23 +343,10 @@ shortcut.add("Shift+Up", function() {
 // Morning evening header
 function greeting(){
   var myDate = new Date();
-  /* hour is before noon */
   if ( myDate.getHours() < 12 ) 
-  {
-  $('#greeting').text("Good Morning!");
-  }
-  else  /* Hour is from noon to 5pm (actually to 5:59 pm) */
-  if ( myDate.getHours() >= 12 && myDate.getHours() <= 17 )
-  {
-  $('#greeting').text("Good Afternoon!");
-  }
-  else  /* the hour is after 5pm, so it is between 6pm and midnight */
-  if ( myDate.getHours() > 17 && myDate.getHours() <= 24 )
-  {
-  $('#greeting').text("Good Evening!");
-  }
-  else  /* the hour is not between 0 and 24, so something is wrong */
-  {
-  $('#greeting').text("I'm not sure what time it is!");
-  }
+  { $('#greeting').text("Good Morning"); }
+  else if ( myDate.getHours() >= 12 && myDate.getHours() <= 17 )
+  { $('#greeting').text("Good Afternoon"); }
+  else if ( myDate.getHours() > 17 && myDate.getHours() <= 24 )
+  { $('#greeting').text("Good Evening"); }
 }
