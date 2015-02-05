d5276576651eb0988e828732b9a42ab8add94d70
Joshua Raichur <hello@joshuaraichur.com>
2015-02-05 09:06:56 +0530

    animating text on index.html

--- resources/js/main.js
+++ resources/js/main.js
@@ -90,6 +90,7 @@ function start(){
 
 $(function(){
   start();
+  greeting();
   $('time').timeago();
   $('.lightbox').fluidbox();
   $(document).ajaxComplete(function() {
@@ -170,34 +171,6 @@ if(desktop.matches) {
                       }), 1000);
 }
 
-// Set the name of the hidden property and the change event for visibility
-var hidden, visibilityChange;
-if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
-  hidden = "hidden";
-  visibilityChange = "visibilitychange";
-} else if (typeof document.mozHidden !== "undefined") {
-  hidden = "mozHidden";
-  visibilityChange = "mozvisibilitychange";
-} else if (typeof document.msHidden !== "undefined") {
-  hidden = "msHidden";
-  visibilityChange = "msvisibilitychange";
-} else if (typeof document.webkitHidden !== "undefined") {
-  hidden = "webkitHidden";
-  visibilityChange = "webkitvisibilitychange";
-}
-
-var title = document.title;
-
-// If the page is hidden, pause the video;
-// if the page is shown, play the video
-function handleVisibilityChange() {
-  if (document[hidden]) {
-    document.title = 'To read: ' + title;
-  } else {
-    document.title = title;
-  }
-}
-
 if(document.getElementById("next_link")) {
   shortcut.add("Left",function() {
     window.open(document.getElementById("next_link").getAttribute('href'),'_self',false);
@@ -211,3 +184,182 @@ if(document.getElementById("prev_link")) {
 shortcut.add("Shift+Up", function() {
   window.open('/','_self',false);
 });
+
+// Animated title
+//set animation timing
+  var animationDelay = 2000,
+    //loading bar effect
+    barAnimationDelay = 3800,
+    barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
+    //letters effect
+    lettersDelay = 50,
+    //type effect
+    typeLettersDelay = 100,
+    selectionDuration = 300,
+    typeAnimationDelay = selectionDuration + 800,
+    //clip effect
+    revealDuration = 600,
+    revealAnimationDelay = 1000;
+
+  initHeadline();
+
+
+  function initHeadline() {
+    //insert <i> element for each letter of a changing word
+    singleLetters($('.cd-headline.letters').find('b'));
+    //initialise headline animation
+    animateHeadline($('.cd-headline'));
+  }
+
+  function singleLetters($words) {
+    $words.each(function(){
+      var word = $(this),
+        letters = word.text().split(''),
+        selected = word.hasClass('is-visible');
+      for (i in letters) {
+        if(word.parents('.rotate-2').length > 0) letters[i] = '<em>' + letters[i] + '</em>';
+        letters[i] = (selected) ? '<i class="in">' + letters[i] + '</i>': '<i>' + letters[i] + '</i>';
+      }
+        var newLetters = letters.join('');
+        word.html(newLetters).css('opacity', 1);
+    });
+  }
+
+  function animateHeadline($headlines) {
+    var duration = animationDelay;
+    $headlines.each(function(){
+      var headline = $(this);
+
+      if(headline.hasClass('loading-bar')) {
+        duration = barAnimationDelay;
+        setTimeout(function(){ headline.find('.cd-words-wrapper').addClass('is-loading') }, barWaiting);
+      } else if (headline.hasClass('clip')){
+        var spanWrapper = headline.find('.cd-words-wrapper'),
+          newWidth = spanWrapper.width() + 10
+        spanWrapper.css('width', newWidth);
+      } else if (!headline.hasClass('type') ) {
+        //assign to .cd-words-wrapper the width of its longest word
+        var words = headline.find('.cd-words-wrapper b'),
+          width = 0;
+        words.each(function(){
+          var wordWidth = $(this).width();
+            if (wordWidth > width) width = wordWidth;
+        });
+        headline.find('.cd-words-wrapper').css('width', width);
+      };
+
+      //trigger animation
+      setTimeout(function(){ hideWord( headline.find('.is-visible').eq(0) ) }, duration);
+    });
+  }
+
+  function hideWord($word) {
+    var nextWord = takeNext($word);
+
+    if($word.parents('.cd-headline').hasClass('type')) {
+      var parentSpan = $word.parent('.cd-words-wrapper');
+      parentSpan.addClass('selected').removeClass('waiting');
+      setTimeout(function(){
+        parentSpan.removeClass('selected');
+        $word.removeClass('is-visible').addClass('is-hidden').children('i').removeClass('in').addClass('out');
+      }, selectionDuration);
+      setTimeout(function(){ showWord(nextWord, typeLettersDelay) }, typeAnimationDelay);
+
+    } else if($word.parents('.cd-headline').hasClass('letters')) {
+      var bool = ($word.children('i').length >= nextWord.children('i').length) ? true : false;
+      hideLetter($word.find('i').eq(0), $word, bool, lettersDelay);
+      showLetter(nextWord.find('i').eq(0), nextWord, bool, lettersDelay);
+
+    }  else if($word.parents('.cd-headline').hasClass('clip')) {
+      $word.parents('.cd-words-wrapper').animate({ width : '2px' }, revealDuration, function(){
+        switchWord($word, nextWord);
+        showWord(nextWord);
+      });
+
+    } else if ($word.parents('.cd-headline').hasClass('loading-bar')){
+      $word.parents('.cd-words-wrapper').removeClass('is-loading');
+      switchWord($word, nextWord);
+      setTimeout(function(){ hideWord(nextWord) }, barAnimationDelay);
+      setTimeout(function(){ $word.parents('.cd-words-wrapper').addClass('is-loading') }, barWaiting);
+
+    } else {
+      switchWord($word, nextWord);
+      setTimeout(function(){ hideWord(nextWord) }, animationDelay);
+    }
+  }
+
+  function showWord($word, $duration) {
+    if($word.parents('.cd-headline').hasClass('type')) {
+      showLetter($word.find('i').eq(0), $word, false, $duration);
+      $word.addClass('is-visible').removeClass('is-hidden');
+
+    }  else if($word.parents('.cd-headline').hasClass('clip')) {
+      $word.parents('.cd-words-wrapper').animate({ 'width' : $word.width() + 10 }, revealDuration, function(){
+        setTimeout(function(){ hideWord($word) }, revealAnimationDelay);
+      });
+    }
+  }
+
+  function hideLetter($letter, $word, $bool, $duration) {
+    $letter.removeClass('in').addClass('out');
+
+    if(!$letter.is(':last-child')) {
+       setTimeout(function(){ hideLetter($letter.next(), $word, $bool, $duration); }, $duration);
+    } else if($bool) {
+       setTimeout(function(){ hideWord(takeNext($word)) }, animationDelay);
+    }
+
+    if($letter.is(':last-child') && $('html').hasClass('no-csstransitions')) {
+      var nextWord = takeNext($word);
+      switchWord($word, nextWord);
+    }
+  }
+
+  function showLetter($letter, $word, $bool, $duration) {
+    $letter.addClass('in').removeClass('out');
+
+    if(!$letter.is(':last-child')) {
+      setTimeout(function(){ showLetter($letter.next(), $word, $bool, $duration); }, $duration);
+    } else {
+      if($word.parents('.cd-headline').hasClass('type')) { setTimeout(function(){ $word.parents('.cd-words-wrapper').addClass('waiting'); }, 200);}
+      if(!$bool) { setTimeout(function(){ hideWord($word) }, animationDelay) }
+    }
+  }
+
+  function takeNext($word) {
+    return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
+  }
+
+  function takePrev($word) {
+    return (!$word.is(':first-child')) ? $word.prev() : $word.parent().children().last();
+  }
+
+  function switchWord($oldWord, $newWord) {
+    $oldWord.removeClass('is-visible').addClass('is-hidden');
+    $newWord.removeClass('is-hidden').addClass('is-visible');
+  }
+
+
+// Morning evening header
+function greeting(){
+  var myDate = new Date();
+  /* hour is before noon */
+  if ( myDate.getHours() < 12 ) 
+  {
+  $('#greeting').text("Good Morning!");
+  }
+  else  /* Hour is from noon to 5pm (actually to 5:59 pm) */
+  if ( myDate.getHours() >= 12 && myDate.getHours() <= 17 )
+  {
+  $('#greeting').text("Good Afternoon!");
+  }
+  else  /* the hour is after 5pm, so it is between 6pm and midnight */
+  if ( myDate.getHours() > 17 && myDate.getHours() <= 24 )
+  {
+  $('#greeting').text("Good Evening!");
+  }
+  else  /* the hour is not between 0 and 24, so something is wrong */
+  {
+  $('#greeting').text("I'm not sure what time it is!");
+  }
+}
