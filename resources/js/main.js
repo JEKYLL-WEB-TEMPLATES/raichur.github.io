e2e3f1b5fa4fb4f6ca541c163e4146257fdf8565
Joshua Raichur <hello@joshuaraichur.com>
2015-03-30 21:18:08 +0530

    finish new navigation menu

--- resources/js/main.js
+++ resources/js/main.js
@@ -339,35 +339,3 @@ $(".navToggle").click(openCloseNav);
 $('#bg-blur').click(function(event) {
   openCloseNav();
 });
-
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('nav').outerHeight();
-
-$(window).scroll(function(event){
-  didScroll = true;
-});
-
-setInterval(function() {
-  if (didScroll) {
-    hasScrolled();
-    didScroll = false;
-  }
-}, 250);
-
-function hasScrolled() {
-  var st = $(this).scrollTop();
-  if(Math.abs(lastScrollTop - st) <= delta)
-  return;
-  if (st > lastScrollTop && st > navbarHeight){
-    $('nav').addClass('nav-hide');
-  } else {
-    if(st + $(window).height() < $(document).height()) {
-      $('nav').removeClass('nav-hide');
-    }
-  }
-
-  lastScrollTop = st;
-}
