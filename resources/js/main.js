c894a60e1b85879ff0007ab4f0da10d6568f23f3
Joshua Raichur <hello@joshuaraichur.com>
2015-04-02 17:12:30 +0530

    fix padding

--- resources/js/main.js
+++ resources/js/main.js
@@ -76,8 +76,10 @@ $(function(){
     jQuery(document).ready(function() {
       var siteUrl = 'http://'+(document.location.hostname||document.location.host);
       $(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click", function(e) {
-        e.preventDefault();
-        History.pushState({}, "", this.pathname);
+        if(!e.hasClass('codeweb')){
+          e.preventDefault();
+          History.pushState({}, "", this.pathname);
+        }
       });
 
       History.Adapter.bind(window, 'statechange', function(){
